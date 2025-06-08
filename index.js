8fbf51a479b2429e265fa5dbc1eb34fdf67b2fd6
Harsha Weerasinghe <hrshweerasinghe@gmail.com>
2025-06-08 20:03:16 +0530

    Initial commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/api/destinations', (req, res) => {
   });
 });
 
-app.get('/', (req, res) => res.send('Hello from Elastic Beanstalk!!V!'));
+app.get('/', (req, res) => res.send('Hello from Elastic Beanstalk!'));
 
 // Only listen ONCE
 app.listen(port, () => {
